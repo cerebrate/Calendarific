0ce4efa1f4488114a3618f279b86084d1b12ce30
Alistair Young <avatar@arkane-systems.net>
2025-12-09 10:49:10 -0600

    Change the default soon to 3 days.

--- custom_components/calendarific/const.py
+++ custom_components/calendarific/const.py
@@ -22,7 +22,7 @@ DEFAULT_ICON_NORMAL = "mdi:calendar-blank"
 DEFAULT_ICON_TODAY = "mdi:calendar-star"
 DEFAULT_ICON_SOON = "mdi:calendar"
 DEFAULT_DATE_FORMAT = "%Y-%m-%d"
-DEFAULT_SOON = 1
+DEFAULT_SOON = 3
 DEFAULT_UNIT_OF_MEASUREMENT = "days"
 
 #Calendar
